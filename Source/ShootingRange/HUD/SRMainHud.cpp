ea43da22b67b3f37dffdc935e43f43f9aacce659
Sebastian Antkowiak <seb.antkowiak@gmail.com>
2023-03-02 00:51:37 +0100

    Add Source files

---
+++ Source/ShootingRange/HUD/SRMainHud.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShootingRange/HUD/SRMainHud.h"
+#include "ShootingRange/HUD/SRInventoryDisplay.h"
+#include "ShootingRange/Weapon/SRInventory.h"
+
+void USRMainHud::Init(USRInventory* Inventory)
+{
+	Inventory->OnSelectedWeaponChanged.AddUObject(this, &USRMainHud::WeaponSelected);
+	Inventory->OnWeaponAdded.AddUObject(this, &USRMainHud::WeaponAdded);
+	Inventory->OnWeaponRemoved.AddUObject(this, &USRMainHud::WeaponRemoved);
+}
